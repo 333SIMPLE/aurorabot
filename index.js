8bf5e96de345b030d554d1061aa0e0c5f192d432
333SIMPLE <55774012+333SIMPLE@users.noreply.github.com>
2019-10-03 20:52:27 -0400

    AURORA 1.1
    Final release of commands for Aurora 1.1.

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const snekfetch = require("snekfetch");
 const randomPuppy = require('random-puppy')
 
 client.once('ready', () => {
-  console.log('Sunset is ready to go.');
+  console.log('Aurora is ready to go.');
   client.user.setActivity('in beta.', {
     type: 'PLAYING'
   });
@@ -29,59 +29,38 @@ client.on('message', message => {
 
       m.edit(`My ping is ${ping}.`)
 });
-    console.log(`COMMAND: ${message.member.displayName} issued ping command.`);
 
-
-    //info command
-  } else if (message.content === `${prefix}info`) {
-    const info = new Discord.RichEmbed()
-    .setTitle('Server Info')
-    .setAuthor(`${member.user.username}`, member.user.avatarURL)
-
-    .setColor("RED")
-    .setTimestamp()
-    .addField("Server Name :", message.guild.name)
-    .addField("Member Count :", message.guild.memberCount)
-    message.channel.send(info);
-    console.log(`COMMAND: ${message.member.displayName} issued info command.`);
-
-
-    //help command
-  } else if (message.content === `${prefix}help`) {
-    console.log(`COMMAND: ${message.member.displayName} issued help command.`);
+}
+ else if (message.content === `${prefix}help`) {
     const help = new Discord.RichEmbed()
-    .setAuthor(`Sunset Help`, message.author.avatarURL)
-
-    .setColor("RED")
+    .setFooter(`Requested by ${message.author.tag}`)
+    .setColor("#a500ff")
     .setTimestamp()
-    .addField("Helpful Commands 👍", `~ping  | Pong?
-    ~info  | Tells you about the server.
+    .addField("Helpful Commands", `~ping  | Pong?
     ~help  | Sends you this message.`)
-    .addField("Fun Commands 😄", `~8ball | Let the Magic 8-ball answer your life questions.
+    .addField("Fun Commands", `~8ball | Let the Magic 8-ball answer your life questions.
     ~kiss  | Give someone a kiss!
     ~kill  | Kill someone!`)
-    .addField(`Media Commands 👀`, `~meme | Fetches you a random dank meme.
-    ~edgymeme | Fetches you a random edgy meme.`)
-    .addField(`NSFW Commands 🔞`, `~porngif | Fetches you Porn GIFS.
-    ~nudes | Fetches you UHD nudes.
-    ~boobs | Fetches you boobs.
-    ~pussy | Fetches you pussy.
-    
-    `)
+    .addField(`Media Commands`, `~meme | Fetches you a random dank meme.`)
     message.channel.send("Sent.");
     message.member.send(help);
   }
 
-// FUN COMMAND SECTION
-// FUN COMMAND SECTION
-// FUN COMMAND SECTION
-
+// fun commands
 
 // 8ball command
   else if (message.content.startsWith(`${prefix}8ball`)) {
-    console.log(`COMMAND: ${message.member.displayName} issued 8ball command.`);
     
-    if(!args[2]) return message.channel.send("Ask a full question.")
+    if(!args[2]) {
+    const eightballerror = new Discord.RichEmbed()
+    .setFooter(`Requested by ${message.author.tag}`)
+    .setTitle('Error')
+    .setDescription("Ask a longer question.")
+    .setColor("#a500ff")
+    .setTimestamp()
+     return message.channel.send(eightballerror)
+    }
+
     let replies = ["Yes.", "No.", "Hell no.", "Hell yeah!", "I don't know", "I really don't know.", "Ask again."]
 
     let result = Math.floor((Math.random() * replies.length));
@@ -89,9 +68,8 @@ client.on('message', message => {
 
     const eightball = new Discord.RichEmbed()
     .setTitle('Magic 8-Ball Results')
-    .setAuthor(`${member.user.username}`, member.user.avatarURL)
 
-    .setColor("RED")
+    .setColor("#a500ff")
     .setTimestamp()
     .addField("Question", question)
     .addField("Answer", replies[result])
@@ -100,54 +78,47 @@ client.on('message', message => {
 
 // kiss command
 else if (message.content.startsWith(`${prefix}kiss`)) {
-console.log(`COMMAND: ${message.member.displayName} issued kiss command.`);
   var kissuser = message.mentions.members.first()
             if(!kissuser){
               const kisserror = new Discord.RichEmbed()
               .setTitle('Error')
-              .setAuthor(`${member.user.username}`, member.user.avatarURL)
               .setDescription(`Mention the user you would like to kiss!`)
-              .setColor("RED")
+              .setColor("#a500ff")
               .setTimestamp()
-                message.channel.send(kisserror)
+              return message.channel.send(kisserror)
             }
             const kissembed = new Discord.RichEmbed()
+            .setFooter(`Requested by ${message.author.tag}`)
             .setTimestamp()
-            .setColor("RED")
-            .setDescription(`${message.author} gave **${kissuser}** a kiss! 💋`)
+            .setColor("#a500ff")
+            .setDescription(`${message.author} gave **${kissuser}** a kiss!`)
             message.channel.send(kissembed)
 }
 
 // kill command
 else if (message.content.startsWith(`${prefix}kill`)) {
-  console.log(`COMMAND: ${message.member.displayName} issued kill command.`);
   var killuser = message.mentions.members.first()
             if(!killuser){
               const killerror = new Discord.RichEmbed()
               .setTitle('Error')
-              .setAuthor(`${member.user.username}`, member.user.avatarURL)
               .setDescription(`Mention the user you would like to kill!`)
-              .setColor("RED")
+              .setColor("#a500ff")
               .setTimestamp()
-                message.channel.send(killerror)
+               return message.channel.send(killerror)
             }
             const killembed = new Discord.RichEmbed()
             .setTimestamp()
-            .setColor("RED")
-            .setDescription(`${message.author} killed **${killuser}**. 🔪`)
+            .setFooter(`Requested by ${message.author.tag}`)
+            .setColor("#a500ff")
+            .setDescription(`${message.author} killed **${killuser}**.`)
             message.channel.send(killembed)
 }
 
-// MEME COMMAND SECTION
-// MEME COMMAND SECTION
-// MEME COMMAND SECTION
-
-//meme
 else if (message.content.startsWith(`${prefix}meme`)) {
-  console.log(`COMMAND: ${message.member.displayName} issued meme command.`);
   randomPuppy(sub)
 var subreddits = [
-  'dankmemes'
+  'dankmemes',
+  'edgymemes'
 ]
 
 var sub = subreddits[Math.round(Math.random() * (subreddits.length - 1))];
@@ -155,152 +126,14 @@ var sub = subreddits[Math.round(Math.random() * (subreddits.length - 1))];
 randomPuppy(sub)
    .then(url => {
        const memeembed = new Discord.RichEmbed()
-           .setColor("RED")
-           .setTitle("Provided by r/dankmemes")
-           .setURL("https://reddit.com/r/dankmemes")
-           .setFooter(`Meme requested by ${message.author.tag}.`)
+           .setColor("#a500ff")
+           .setTitle(`Provided by r/${sub}`)
+           .setURL(`https://reddit.com/r/${sub}`)
+           .setFooter(`Requested by ${message.author.tag}`)
            .setTimestamp()
            .setImage(url);
        message.channel.send(memeembed)
    })
   }
-
-
-  // edgymeme
-  else if (message.content.startsWith(`${prefix}edgymeme`)) {
-    console.log(`COMMAND: ${message.member.displayName} issued edgymeme command.`);
-    randomPuppy(sub)
-  var subreddits2 = [
-    'edgymemes'
-  ]
-  
-  var sub2 = subreddits2[Math.round(Math.random() * (subreddits2.length - 1))];
-  
-  randomPuppy(sub2)
-     .then(url => {
-         const meme2embed = new Discord.RichEmbed()
-         .setColor("RED")
-         .setTitle("Provided by r/EdgyMemes")
-         .setURL("https://reddit.com/r/edgymemes")
-         .setFooter(`Meme requested by ${message.author.tag}.`)
-         .setTimestamp()
-         .setImage(url);
-         message.channel.send(meme2embed)
-     })
-    }
-
-
-// NSFW SECTION
-// NSFW SECTION
-// NSFW SECTION
-
-
-else if (message.content.startsWith(`${prefix}porngif`)) {
-  const nsfwchannelerror = new Discord.RichEmbed()
-  .setTitle('Error')
-  .setDescription(`You can only use this command in NSFW channels!`)
-  .setColor("RED")
-  .setTimestamp()
-if (!message.channel.nsfw) return message.channel.send(nsfwchannelerror);
-
-  var nsfwgifsubreddits = [
-    'porngifs',
-]
-var nsfwgifsub = nsfwgifsubreddits[Math.round(Math.random() * (nsfwgifsubreddits.length - 1))];
-
-randomPuppy(nsfwgifsub)
-        .then(url => {
-            const nsfwgifembed = new Discord.RichEmbed()
-                .setColor("RED")
-                .setAuthor(`Requested by ${message.author.tag}`, message.author.avatarURL)
-                .setFooter("Provided by r/porngifs")
-                .setTimestamp()
-                .setImage(url);
-            message.channel.send(nsfwgifembed)
-  })
-}
-
-// hdporn
-else if (message.content.startsWith(`${prefix}nudes`)) {
-if (!message.channel.nsfw) return message.channel.send(nsfwchannelerror);
-
-  var nsfwhdsubreddits = [
-    'UHDnsfw',
-]
-var nsfwhdsub = nsfwhdsubreddits[Math.round(Math.random() * (nsfwhdsubreddits.length - 1))];
-
-randomPuppy(nsfwhdsub)
-        .then(url => {
-          const nsfwhdembed = new Discord.RichEmbed()
-          .setColor("RED")
-          .setAuthor(`Requested by ${message.author.tag}`, message.author.avatarURL)
-          .setFooter("Provided by r/UHDnsfw")
-          .setTimestamp()
-          .setImage(url);
-            message.channel.send(nsfwhdembed)
-  })
-}
-
-
-else if (message.content.startsWith(`${prefix}boobs`)) {
-  if (!message.channel.nsfw) return message.channel.send(nsfwchannelerror);
-  
-    var boobsubreddits = [
-      'boobies',
-  ]
-  var boobsub = boobsubreddits[Math.round(Math.random() * (boobsubreddits.length - 1))];
-  
-  randomPuppy(boobsub)
-          .then(url => {
-            const boobembed = new Discord.RichEmbed()
-            .setColor("RED")
-            .setAuthor(`Requested by ${message.author.tag}`, message.author.avatarURL)
-            .setFooter("Provided by r/boobies")
-            .setTimestamp()
-            .setImage(url);
-              message.channel.send(boobembed)
-    })
-  }
-
-
-  else if (message.content.startsWith(`${prefix}pussy`)) {
-    if (!message.channel.nsfw) return message.channel.send(nsfwchannelerror);
-    
-      var pussysubreddits = [
-        'pussy',
-    ]
-    var pussysub = pussysubreddits[Math.round(Math.random() * (pussysubreddits.length - 1))];
-    
-    randomPuppy(pussysub)
-            .then(url => {
-              const pussyembed = new Discord.RichEmbed()
-              .setColor("RED")
-              .setAuthor(`Requested by ${message.author.tag}`, message.author.avatarURL)
-              .setFooter("Provided by r/pussy")
-              .setTimestamp()
-              .setImage(url);
-                message.channel.send(pussyembed)
-      })
-    }
-
-    else if (message.content.startsWith(`${prefix}boobs`)) {
-      if (!message.channel.nsfw) return message.channel.send(nsfwchannelerror);
-      
-        var boobsubreddits = [
-          'boobies',
-      ]
-      var boobsub = boobsubreddits[Math.round(Math.random() * (boobsubreddits.length - 1))];
-      
-      randomPuppy(boobsub)
-              .then(url => {
-                const boobembed = new Discord.RichEmbed()
-                .setColor("RED")
-                .setAuthor(`Requested by ${message.author.tag}`, message.author.avatarURL)
-                .setFooter("Provided by r/boobies")
-                .setTimestamp()
-                .setImage(url);
-                  message.channel.send(boobembed)
-        })
-      }
 });
 client.login(token);

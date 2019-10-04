b78dc7dffd2970ae8a68a53e24e6cbe6a0ff5a83
333SIMPLE <55774012+333SIMPLE@users.noreply.github.com>
2019-10-03 21:18:30 -0400

    Avatar Command (1.1.1)
    Release of the avatar command.

--- index.js
+++ index.js
@@ -37,7 +37,8 @@ client.on('message', message => {
     .setColor("#a500ff")
     .setTimestamp()
     .addField("Helpful Commands", `~ping  | Pong?
-    ~help  | Sends you this message.`)
+    ~help  | Sends you this message.
+    ~av    | Fetches your avatar, or someone you mention.`)
     .addField("Fun Commands", `~8ball | Let the Magic 8-ball answer your life questions.
     ~kiss  | Give someone a kiss!
     ~kill  | Kill someone!`)
@@ -133,7 +134,41 @@ randomPuppy(sub)
            .setTimestamp()
            .setImage(url);
        message.channel.send(memeembed)
+       
    })
   }
+
+  else if (message.content.startsWith(`${prefix}av`)) {
+    regex = /\?=size[0-9]{1,45}$/g;
+    memberMention = null;
+    if (message.mentions.members) {
+        memberMention = message.mentions.members.first()
+    }
+    if (message.mentions.users) {
+        userMention = message.mentions.users.first()
+    }
+    if (!memberMention) {
+        if (message.author.displayAvatarURL.indexOf(/\.gif\?size=[0-9]{1,49}$/g)) {
+          const av = new Discord.RichEmbed()
+          .setAuthor(`${message.author.username}'s Avatar`)
+          .setColor("#a500ff")
+          .setImage(message.author.avatarURL)
+          .setTimestamp()
+            return message.channel.send(av)
+        }
+        return message.channel.send(message.author.displayAvatarURL)
+    }
+    if (memberMention.user.displayAvatarURL.indexOf(/\.gif\?size=[0-9]{1,49}$/g)) {
+      const mentionav = new Discord.RichEmbed()
+      .setAuthor(`${memberMention.user.username}'s Avatar`)
+      .setColor(`#a500ff`)
+      .setImage(memberMention.user.avatarURL)
+      .setTimestamp()
+
+        return message.channel.send(mentionav)
+    }
+    message.channel.send(memberMention.user.displayAvatarURL)
+  }
+
 });
 client.login(token);

fb012697c5fc520a66e9e55dadd14f790d7ac0cb
LogixHacks <52875890+logixforeverrr@users.noreply.github.com>
2019-10-04 15:47:30 +0100

    Update Music Bot
    Added music bot

--- index.js
+++ index.js
@@ -3,13 +3,31 @@ var { prefix, token } = require('./config.json');
 const client = new Discord.Client();
 const snekfetch = require("snekfetch");
 const randomPuppy = require('random-puppy')
+const YTDL = require("ytdl-core")
+const getYouTubeID = require("get-youtube-id")
+const fetchVideoInfo = require("youtube-info")
+const servers = require("net")
+
+function play(connection, message) {
+  var server = servers[message.guild.id];
+  server.dispatcher = connection.playStream(YTDL(server.queue[0], { filter: "audioonly" }));
+
+  server.queue.shift();
+
+  server.dispatcher.on("end", function () {
+    if (server.queue[0]) play(connection, message);
+    else connection.disconnect();
+
+  })
+}
+
 
 client.once('ready', () => {
   console.log('Aurora is ready to go.');
   client.user.setActivity('in beta.', {
     type: 'PLAYING'
   });
-}) 
+})
 
 
 client.on('message', message => {
@@ -17,49 +35,49 @@ client.on('message', message => {
   let member = message.member
 
 
-// USEFUL COMMAND AREA  
-// USEFUL COMMAND AREA
-// USEFUL COMMAND AREA
+  // USEFUL COMMAND AREA  
+  // USEFUL COMMAND AREA
+  // USEFUL COMMAND AREA
 
 
-// ping command
-   if (message.content === `${prefix}ping`) {
+  // ping command
+  if (message.content === `${prefix}ping`) {
     message.channel.send('Pinging...').then(m => {
       let ping = m.createdTimestamp - message.createdTimestamp
 
       m.edit(`My ping is ${ping}.`)
-});
+    });
 
-}
- else if (message.content === `${prefix}help`) {
+  }
+  else if (message.content === `${prefix}help`) {
     const help = new Discord.RichEmbed()
-    .setFooter(`Requested by ${message.author.tag}`)
-    .setColor("#a500ff")
-    .setTimestamp()
-    .addField("Helpful Commands", `~ping  | Pong?
+      .setFooter(`Requested by ${message.author.tag}`)
+      .setColor("#a500ff")
+      .setTimestamp()
+      .addField("Helpful Commands", `~ping  | Pong?
     ~help  | Sends you this message.
     ~av    | Fetches your avatar, or someone you mention.`)
-    .addField("Fun Commands", `~8ball | Let the Magic 8-ball answer your life questions.
+      .addField("Fun Commands", `~8ball | Let the Magic 8-ball answer your life questions.
     ~kiss  | Give someone a kiss!
     ~kill  | Kill someone!`)
-    .addField(`Media Commands`, `~meme | Fetches you a random dank meme.`)
+      .addField(`Media Commands`, `~meme | Fetches you a random dank meme.`)
     message.channel.send("Sent.");
     message.member.send(help);
   }
 
-// fun commands
+  // fun commands
 
-// 8ball command
+  // 8ball command
   else if (message.content.startsWith(`${prefix}8ball`)) {
-    
-    if(!args[2]) {
-    const eightballerror = new Discord.RichEmbed()
-    .setFooter(`Requested by ${message.author.tag}`)
-    .setTitle('Error')
-    .setDescription("Ask a longer question.")
-    .setColor("#a500ff")
-    .setTimestamp()
-     return message.channel.send(eightballerror)
+
+    if (!args[2]) {
+      const eightballerror = new Discord.RichEmbed()
+        .setFooter(`Requested by ${message.author.tag}`)
+        .setTitle('Error')
+        .setDescription("Ask a longer question.")
+        .setColor("#a500ff")
+        .setTimestamp()
+      return message.channel.send(eightballerror)
     }
 
     let replies = ["Yes.", "No.", "Hell no.", "Hell yeah!", "I don't know", "I really don't know.", "Ask again."]
@@ -68,107 +86,143 @@ client.on('message', message => {
     let question = args.slice(1).join(" ")
 
     const eightball = new Discord.RichEmbed()
-    .setTitle('Magic 8-Ball Results')
+      .setTitle('Magic 8-Ball Results')
 
-    .setColor("#a500ff")
-    .setTimestamp()
-    .addField("Question", question)
-    .addField("Answer", replies[result])
+      .setColor("#a500ff")
+      .setTimestamp()
+      .addField("Question", question)
+      .addField("Answer", replies[result])
     message.channel.send(eightball)
   }
 
-// kiss command
-else if (message.content.startsWith(`${prefix}kiss`)) {
-  var kissuser = message.mentions.members.first()
-            if(!kissuser){
-              const kisserror = new Discord.RichEmbed()
-              .setTitle('Error')
-              .setDescription(`Mention the user you would like to kiss!`)
-              .setColor("#a500ff")
-              .setTimestamp()
-              return message.channel.send(kisserror)
-            }
-            const kissembed = new Discord.RichEmbed()
-            .setFooter(`Requested by ${message.author.tag}`)
-            .setTimestamp()
-            .setColor("#a500ff")
-            .setDescription(`${message.author} gave **${kissuser}** a kiss!`)
-            message.channel.send(kissembed)
-}
+  // kiss command
+  else if (message.content.startsWith(`${prefix}kiss`)) {
+    var kissuser = message.mentions.members.first()
+    if (!kissuser) {
+      const kisserror = new Discord.RichEmbed()
+        .setTitle('Error')
+        .setDescription(`Mention the user you would like to kiss!`)
+        .setColor("#a500ff")
+        .setTimestamp()
+      return message.channel.send(kisserror)
+    }
+    const kissembed = new Discord.RichEmbed()
+      .setFooter(`Requested by ${message.author.tag}`)
+      .setTimestamp()
+      .setColor("#a500ff")
+      .setDescription(`${message.author} gave **${kissuser}** a kiss!`)
+    message.channel.send(kissembed)
+  }
 
-// kill command
-else if (message.content.startsWith(`${prefix}kill`)) {
-  var killuser = message.mentions.members.first()
-            if(!killuser){
-              const killerror = new Discord.RichEmbed()
-              .setTitle('Error')
-              .setDescription(`Mention the user you would like to kill!`)
-              .setColor("#a500ff")
-              .setTimestamp()
-               return message.channel.send(killerror)
-            }
-            const killembed = new Discord.RichEmbed()
-            .setTimestamp()
-            .setFooter(`Requested by ${message.author.tag}`)
-            .setColor("#a500ff")
-            .setDescription(`${message.author} killed **${killuser}**.`)
-            message.channel.send(killembed)
-}
+  // kill command
+  else if (message.content.startsWith(`${prefix}kill`)) {
+    var killuser = message.mentions.members.first()
+    if (!killuser) {
+      const killerror = new Discord.RichEmbed()
+        .setTitle('Error')
+        .setDescription(`Mention the user you would like to kill!`)
+        .setColor("#a500ff")
+        .setTimestamp()
+      return message.channel.send(killerror)
+    }
+    const killembed = new Discord.RichEmbed()
+      .setTimestamp()
+      .setFooter(`Requested by ${message.author.tag}`)
+      .setColor("#a500ff")
+      .setDescription(`${message.author} killed **${killuser}**.`)
+    message.channel.send(killembed)
+  }
 
-else if (message.content.startsWith(`${prefix}meme`)) {
-  randomPuppy(sub)
-var subreddits = [
-  'dankmemes',
-  'edgymemes'
-]
+  else if (message.content.startsWith(`${prefix}meme`)) {
+    randomPuppy(sub)
+    var subreddits = [
+      'dankmemes',
+      'edgymemes'
+    ]
 
-var sub = subreddits[Math.round(Math.random() * (subreddits.length - 1))];
+    var sub = subreddits[Math.round(Math.random() * (subreddits.length - 1))];
 
-randomPuppy(sub)
-   .then(url => {
-       const memeembed = new Discord.RichEmbed()
-           .setColor("#a500ff")
-           .setTitle(`Provided by r/${sub}`)
-           .setURL(`https://reddit.com/r/${sub}`)
-           .setFooter(`Requested by ${message.author.tag}`)
-           .setTimestamp()
-           .setImage(url);
-       message.channel.send(memeembed)
-       
-   })
+    randomPuppy(sub)
+      .then(url => {
+        const memeembed = new Discord.RichEmbed()
+          .setColor("#a500ff")
+          .setTitle(`Provided by r/${sub}`)
+          .setURL(`https://reddit.com/r/${sub}`)
+          .setFooter(`Requested by ${message.author.tag}`)
+          .setTimestamp()
+          .setImage(url);
+        message.channel.send(memeembed)
+
+      })
   }
 
   else if (message.content.startsWith(`${prefix}av`)) {
     regex = /\?=size[0-9]{1,45}$/g;
     memberMention = null;
     if (message.mentions.members) {
-        memberMention = message.mentions.members.first()
+      memberMention = message.mentions.members.first()
     }
     if (message.mentions.users) {
-        userMention = message.mentions.users.first()
+      userMention = message.mentions.users.first()
     }
     if (!memberMention) {
-        if (message.author.displayAvatarURL.indexOf(/\.gif\?size=[0-9]{1,49}$/g)) {
-          const av = new Discord.RichEmbed()
+      if (message.author.displayAvatarURL.indexOf(/\.gif\?size=[0-9]{1,49}$/g)) {
+        const av = new Discord.RichEmbed()
           .setAuthor(`${message.author.username}'s Avatar`)
           .setColor("#a500ff")
           .setImage(message.author.avatarURL)
           .setTimestamp()
-            return message.channel.send(av)
-        }
-        return message.channel.send(message.author.displayAvatarURL)
+        return message.channel.send(av)
+      }
+      return message.channel.send(message.author.displayAvatarURL)
     }
     if (memberMention.user.displayAvatarURL.indexOf(/\.gif\?size=[0-9]{1,49}$/g)) {
       const mentionav = new Discord.RichEmbed()
-      .setAuthor(`${memberMention.user.username}'s Avatar`)
-      .setColor(`#a500ff`)
-      .setImage(memberMention.user.avatarURL)
-      .setTimestamp()
+        .setAuthor(`${memberMention.user.username}'s Avatar`)
+        .setColor(`#a500ff`)
+        .setImage(memberMention.user.avatarURL)
+        .setTimestamp()
 
-        return message.channel.send(mentionav)
+      return message.channel.send(mentionav)
     }
     message.channel.send(memberMention.user.displayAvatarURL)
   }
+  // play command 
 
+  else if (message.content.startsWith(`${prefix}play`)) {
+    if (!args[1]) {
+      var server = servers[message.guild.id];
+      message.channel.send("Please Provide a Link")
+      return;
+    }
+
+    if (!message.member.voiceChannel) {
+      message.channel.send("You must be in a voice channel ")
+      return;
+    }
+
+    if (!servers[message.guild.id]) servers[message.guild.id] = {
+      queue: []
+    };
+    var server = servers[message.guild.id];
+    
+    server.queue.push(args[1]);
+
+    if (!message.guild.voiceConnection) message.member.voiceChannel.join().then(function (connection) {
+      play(connection, message)
+    });
+  
+    else if (message.content.startsWith(`${prefix}skip`)) {
+      var server = servers[message.guild.id];
+      message.channel.send("Ok Skipped")
+
+      if (server.dispatcher) server.dispatcher.end();
+    }
+  } 
+  else if (message.content.startsWith(`${prefix}stop`)) {
+    var server = servers[message.guild.id];
+    if (message.guild.voiceConnection) message.guild.voiceConnection.disconnect();
+    message.channel.send("Ok Stopped")
+  }
 });
 client.login(token);
